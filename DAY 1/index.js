376b0894b44040f1d0c36c6ceef29db6ef9b41cd
ABC <mayukc55@gmail.com>
2024-04-29 19:32:45 +0530

    day 1 js

---
+++ DAY 1/index.js
@@ -0,0 +1,64 @@
+console.log("Hello World");
+
+console.log("Mayukhjit Chakraborty");
+
+console.log(2+5);
+
+let number1=13;
+let number2=29.45;
+let number3=null;
+
+console.log(number3);
+console.log(number2);
+console.log(number1);
+
+let name1="mayukhjit";
+let name2='Debrup';
+let name3=`shaina`;
+
+console.log(name1);
+console.log(name2);
+console.log(name3);
+
+let char1="m";
+let char2='p';
+let char3=`g`;
+
+console.log(char1);
+console.log(char2);
+console.log(char3);
+
+let check1=true;
+let check2=false;
+
+console.log(check1);
+console.log(check2 );
+
+
+// Operator 
+
+// Arihmetic Operator
+
+let a=64;
+let b=31;
+
+
+let a1=a+b;
+let a2=a-b;
+let a3=a*b;
+let a4=a/b;
+let a5=a%b;
+
+console.log(a1);
+console.log(a2);
+console.log(a3);
+console.log(a4);
+console.log(a5);
+console.log(2**3);
+
+let b1=5;
+let b2=4;
+
+let b3=b1 ** b2;
+console.log(b3);
+
