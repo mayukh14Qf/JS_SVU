4b339dbe2972c1bb428b428127f32117166490ea
ABC <mayukc55@gmail.com>
2024-04-30 21:54:31 +0530

    Day 2

---
+++ DAY 2/index.js
@@ -0,0 +1,37 @@
+let p=2**3/4+34*6%5;
+console.log(p);
+let q=2**4**2**2;
+console.log(q);
+
+let nameTitle=`Mayukhjit`;
+console.log(nameTitle.length);
+console.log(nameTitle[3]);
+console.log(nameTitle[nameTitle.length-1]);
+// or
+console.log(nameTitle.charAt(0));
+
+let var1=10;
+
+var1 +=4;
+var1++;
+var1 *=2;
+var1 -=15;
+console.log(var1--);
+console.log(var1);
+console.log(--var1);
+
+let name2="Chakraborty";
+console.log(nameTitle+" "+name2);
+
+console.log(nameTitle+var1);
+
+console.log('2'+'4');
+console.log(2+4);
+
+console.log(('2+3+6#%!'));
+
+let name3="Xyz";
+
+console.log(`my name is `+name3);
+console.log(`my name is ${nameTitle}`);
+
