0e760532a075ddbd17bc69a3a6e2a4d638d7195b
ABC <mayukc55@gmail.com>
2024-05-02 12:55:00 +0530

    DAY 3 JS

---
+++ DAY 3/index.js
@@ -0,0 +1,27 @@
+console.log("hello :((");
+console.log("this is day 3!!!");
+
+console.log(5==5);
+console.log(5===5);
+
+console.log(5=='5');
+console.log(5==='5');
+
+console.log();
+
+console.log(true=='true'); // false
+console.log(true=='1'); //true
+console.log(true==='1'); //false
+console.log(true===1); //false
+
+console.log(true=='false');
+console.log(true==='true');
+console.log(true==false);
+console.log(true===false);
+console.log(0=='true');
+console.log(0==='true');
+console.log(0=='false');
+console.log(""==true);
+console.log(""==false);
+console.log(""===false);
+console.log(""==true);
