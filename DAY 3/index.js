557b217077f036a0fab030dcc6ff3dade719180c
ABC <mayukc55@gmail.com>
2024-05-02 13:03:46 +0530

    edited

--- DAY 3/index.js
+++ DAY 3/index.js
@@ -14,14 +14,14 @@ console.log(true=='1'); //true
 console.log(true==='1'); //false
 console.log(true===1); //false
 
-console.log(true=='false');
-console.log(true==='true');
-console.log(true==false);
-console.log(true===false);
-console.log(0=='true');
-console.log(0==='true');
-console.log(0=='false');
-console.log(""==true);
-console.log(""==false);
-console.log(""===false);
-console.log(""==true);
+console.log(true=='false'); //false
+console.log(true==='true'); // false
+console.log(true==false); //false
+console.log(true===false); // false
+console.log(0=='true'); // false 
+console.log(0==='true'); // false
+console.log(0=='false'); // false
+console.log(""==true); //false
+console.log(""==false); //true
+console.log(""===false); //false
+console.log(""==true); //false
